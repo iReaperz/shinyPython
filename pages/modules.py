99c26a4a92458d80838b55a27978ba8667fcfde5
Noy Simonyan <ireaperz@mail.ru>
2024-02-22 16:01:16 +0400

    UI/Server Changes for First Plot

--- pages/modules.py
+++ pages/modules.py
@@ -5,6 +5,8 @@ from pages.plots import plot_auc_curve, plot_precision_recall_curve, plot_score_
 
 from shiny import Inputs, Outputs, Session, module, render, ui, reactive
 from shinywidgets import output_widget, render_widget  
+from shiny.types import ImgData
+from htmltools import HTML, div
 
 adlbc = pd.read_csv("raw/adlbc.csv")
 adsl = pd.read_csv("raw/adsl.csv")
@@ -29,12 +31,20 @@ def training_ui():
                         "param2",
                         "Parameter Category 2:",
                         choices=[value for value in adlbc["paramcd"].unique() if not value.startswith('_')], selected= "AST", width="500px"
-                    )
+                    ),
+                    ui.HTML("<div class ='bottomNav'>       \
+                                <p class ='pBottom'>        \
+                                    Follow Us:              \
+                                    <a href='https://www.google.com' class = 'img'> <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/108px-LinkedIn_icon.svg.png' width='15' height='15'> </a>\
+                                    <a href='https://www.google.com' class = 'img'> <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/900px-Octicons-mark-github.svg.png?20180806170715'  width='15' height='15'> </a>\
+                                    <a href='https://www.google.com' class = 'img'> <img src='https://upload.wikimedia.org/wikipedia/commons/e/ec/Medium_logo_Monogram.svg' width='15' height='15'> </a>\
+                                </p>                        \
+                            </div>")
             ),
             ui.card(
                     output_widget("series"),
                     style=" height: 900px;  border: none;"
-                ),col_widths=(3,9)
+                ),col_widths=(2,10)
         )
     )
 
@@ -47,11 +57,27 @@ def training_server(
 ):
     @reactive.Calc()
     def filtered_data() -> pd.DataFrame:
-        return df.loc[df["usubjid"] == input.usubjid()]
+        return df
 
     @render_widget
     def series():
         return plot_auc_curve(filtered_data(), input.usubjid, input.param1, input.param2)
+    
+    @render.image
+    def linked():
+        img: ImgData = {"src": str("assets/linked.png"), "width": "20px", "height":"20px"}
+        return img
+    
+    @render.image
+    def git():
+        img: ImgData = {"src": str("assets/git.png"), "width": "20px", "height":"20px"}
+        return img
+    
+    @render.image
+    def med():
+        img: ImgData = {"src": str("assets/med.png"), "width": "20px", "height":"20px"}
+        return img
+    
 
 
 @module.ui
--- pages/plots.py
+++ pages/plots.py
@@ -26,13 +26,13 @@ def plot_auc_curve(df: DataFrame, subjid, first_val, second_val):
 
     filtered_raw = df[(df["paramcd"].isin([first_val.get(), second_val.get()])) & 
                        (df["avisitn"] >= 0) & (df["saffl"] == "Y")]
-    print(first_val.get(), second_val.get())
+
     # Preparing data for further analysis   
     uln_values = filtered_raw.groupby("paramcd").agg(minUALN=("a1hi", "min"), maxUALN=("a1hi", "max")).reset_index()
     alt_min = uln_values.loc[uln_values["paramcd"] == first_val.get(), "minUALN"].values[0]
     ast_min = uln_values.loc[uln_values["paramcd"] == second_val.get(), "minUALN"].values[0]
-    print(alt_min,ast_min)
 
+    filtered_raw = filtered_raw[filtered_raw["usubjid"] == subjid.get()]
     table_data = filtered_raw.groupby(["paramcd", "ady"]).sum("chg").reset_index().pivot(index="paramcd", columns="ady", values="chg")
     colors = ["purple", "darkgreen"]
     line_types = ["dash", "longdash"]
@@ -47,16 +47,18 @@ def plot_auc_curve(df: DataFrame, subjid, first_val, second_val):
             name=paramcd
         ))
 
-
     fig.add_hline(y=alt_min,line_width=1, line_dash="solid", line_color="gray", opacity=0.7)
     fig.add_hline(y=ast_min,line_width=1, line_dash="solid", line_color="gray", opacity=0.7)
-
-    fig.update_layout(height =800, template = "simple_white",
+    
+    fig.update_layout(  template = "plotly_white",
+                        margin=dict(l=60, t=80),
                         legend = dict(orientation = "h", 
                                     title = "", x = 0.5, y = 0.155,
-                                    font = dict(size = 12, color = "black"), bordercolor = "black", borderwidth = 1), 
+                                    font = dict(size = 12, color = "black"), bordercolor = "black", borderwidth = 1),
+                        yaxis=dict(domain=[0.25, 1]),
+                        xaxis=dict(tickmode='array', tickvals=[str(val) for val in filtered_raw["ady"][filtered_raw["paramcd"] == paramcd].unique()]),
                         title_text=f"<b>{first_val.get()} and {second_val.get()} Results Over Time. (Safety Analysis Set)</b>", title_x=0.535,
-                        title_font=dict(size=24, family="Balto"))
+                        title_font=dict(size=20, family="Balto"))
 
     fig.add_trace(go.Table(
         header=dict(values=[], fill=dict(color='rgba(0, 0, 0, 0)')),
@@ -66,29 +68,18 @@ def plot_auc_curve(df: DataFrame, subjid, first_val, second_val):
         ),
         domain=dict(y=[0, 0.1])  # Adjust the y values as needed
     ))
-
-    # Adjust the yaxis domain to leave space for the table
-    fig.update_layout(
-        yaxis=dict(domain=[0.25, 1])  # Adjust the values as needed
-    )
-
-        
-    fig.update_layout(
-        xaxis=dict(
-            tickmode='array',
-            tickvals=[str(val) for val in filtered_raw["ady"][filtered_raw["paramcd"] == paramcd].unique()]
-        )
-    )
-
+    
+    fig.update_xaxes(showline=True, linewidth=2, linecolor='gray')
+    fig.update_yaxes(showline=True, linewidth=2, linecolor='gray')
 
     fig.add_annotation(x=1, y=alt_min + 0.21,text=f'{first_val.get()} ULN',showarrow=False,font=dict(size=8),xref="paper")
     fig.add_annotation(x=1,y=ast_min + 0.21, text=f'{second_val.get()} ULN', showarrow=False, font=dict(size=8), xref="paper")
     fig.add_annotation(showarrow=False,xref='paper', x=0, yref='paper',y=0.012,text=f"{second_val.get()}",font=dict(size=12))
     fig.add_annotation(showarrow=False,xref='paper', x=0, yref='paper',y=0.05,text=f"{first_val.get()}",font=dict(size=12))
     fig.add_annotation(showarrow=False,xref='paper', x=0.03, yref='paper',y=0.1,text="Change from Baseline",font=dict(size=14))
-    fig.add_annotation(showarrow=False,xref='paper', x=-0.06, yref='paper',y=0.65,textangle=-90,text="Analysis value",font=dict(size=18))
-    fig.add_annotation(showarrow=False,xref='paper', x=0.535, yref='paper',y= 0.17,text="Study day relative to treatment start day",font=dict(size=18))
-    fig.add_annotation(showarrow=False,xref='paper', x=0.535, yref='paper',y= 1.02,text=f'Usubjid: {subjid.get()}, Treatment: {filtered_raw["trta"].unique()[0]}',font=dict(size=18))
+    fig.add_annotation(showarrow=False,xref='paper', x=-0.05, yref='paper',y=0.65,textangle=-90,text="Analysis value",font=dict(size=18))
+    fig.add_annotation(showarrow=False,xref='paper', x=0.55, yref='paper',y= 0.18,text="Study day relative to treatment start day",font=dict(size=18))
+    fig.add_annotation(showarrow=False,xref='paper', x=0.535, yref='paper',y= 1.04,text=f'Usubjid: {subjid.get()}, Treatment: {filtered_raw["trta"].unique()[0]}',font=dict(size=16))
 
     return fig
 
